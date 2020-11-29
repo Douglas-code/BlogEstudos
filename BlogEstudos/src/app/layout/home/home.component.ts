cd9e46b13d8bc7edac11f57e1c01113c3ed7a202
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-11-29 18:02:20 -0300

    criando metodo de logout e criando navbar

--- BlogEstudos/src/app/layout/home/home.component.ts
+++ BlogEstudos/src/app/layout/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  id = window.localStorage.getItem('id');
+  nome = window.localStorage.getItem('nome');
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    console.log(this.nome, this.id)
+  }
+
+  Logout()
+  {
+    localStorage.clear();
+
+    this.router.navigate(['/login']);
   }
 
 }
