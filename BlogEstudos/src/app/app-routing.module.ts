a6b150db565e8b8538686b6516d5ffe6b766e793
Douglas Rodrigues <douglasrodrigues20161@outlook.com>
2020-11-29 12:22:00 -0300

    Configurando rotas da aplicacao

--- BlogEstudos/src/app/app-routing.module.ts
+++ BlogEstudos/src/app/app-routing.module.ts
@@ -1,7 +1,28 @@
+import { AuthGuard } from './account/shared/auth.guard';
+import { CreateAccountComponent } from './account/create-account/create-account.component';
+import { LoginComponent } from './account/login/login.component';
+import { AuthenticationComponent } from './layout/authentication/authentication.component';
+import { HomeComponent } from './layout/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [],
+    canActivate: [AuthGuard]
+  }, 
+  { 
+    path: '',
+    component: AuthenticationComponent,
+    children: [
+      { path: '', redirectTo: 'login', pathMatch: 'full' },
+      { path: 'login', component: LoginComponent },
+      { path: 'create-account', component: CreateAccountComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
